5f48ca89a3a2f27891ef03e77f541c30dcf984b8
theapi <petermclarke@gmail.com>
2015-10-03 18:12:58 +0100

    Attempt at a testbench

---
+++ projects/01/And/my_and_tb.v
@@ -0,0 +1,27 @@
+`include "my_and.v"
+module my_and_tb();
+
+// Declare inputs as regs and outputs as wires
+reg a, b;
+wire out;
+
+// Initialize all variables
+initial begin        
+  $monitor ("a=%b b=%b out%b", 
+	    a, b, out);	
+
+  a=0;
+  b=0;
+  
+  #10 a=1;
+  #10 b=1;
+  #10 a=0;
+  #10 b=0;
+  #10 a=1;
+  #10 b=1;
+
+  #10 $finish;      // Terminate simulation
+
+end
+
+endmodule
