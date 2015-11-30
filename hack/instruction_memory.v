9956d3c550fc193e0d3183be58e241c187dac77e
theapi <petermclarke@gmail.com>
2015-11-30 18:08:05 +0000

    Memory size fix

--- hack/instruction_memory.v
+++ hack/instruction_memory.v
@@ -16,7 +16,7 @@ module instruction_memory(
     
     // 32k is too much for internal ram on cyclone IV
     //reg [15:0] mem [16383:0] /* synthesis ram_init_file = "program.mif" */; // 16K 16-bit RAM
-    reg [15:0] mem [8192:0] /* synthesis ram_init_file = "program.mif" */; // 8K 16-bit RAM
+    reg [15:0] mem [8191:0] /* synthesis ram_init_file = "program.mif" */; // 8K 16-bit RAM
     
     always @ (posedge clk) begin
         if (we)
