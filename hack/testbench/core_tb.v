9193d125c56a836478a50ace0a9b69dc968017e1
Peter Clarke <petermclarke@gmail.com>
2015-12-19 00:01:14 +0000

    Rect test WIP

--- hack/testbench/core_tb.v
+++ hack/testbench/core_tb.v
@@ -2,13 +2,42 @@
 
 `timescale 10ns/10ns
 
-module data_int(
+module data_mux(
     input clk,
-    input [15:0] in,
-    input [14:0] address,
-    input load,
-    output [15:0] out
+    input sel,
+    
+    input [15:0] a_in,
+    input [14:0] a_address,
+    input a_we,
+    
+    input [15:0] b_in,
+    input [14:0] b_address,
+    input b_we,
+    
+    output [15:0] out,
+    output write,
+    output [14:0] address
 );
+
+    reg [15:0] r_out;
+    reg r_write;
+    reg [14:0] r_address;
+
+    assign out = r_out;
+    assign write = r_write;
+    assign address = r_address;
+            
+    always @ (posedge clk) begin
+        if (sel) begin
+            r_out <= a_in; 
+            r_write <= a_we;
+            r_address <= a_address;
+        end else begin
+            r_out <= b_in; 
+            r_write <= b_we;
+            r_address <= b_address;
+        end
+    end
 
 endmodule
 
@@ -17,10 +46,18 @@ module core_tb();
 	// Declare inputs as regs and outputs as wires
 	reg clk, reset;
     wire [15:0] inM;
-    reg [15:0] instruction;
+    wire [15:0] instruction;
     
     reg [7:0] keyboard;
 
+    reg [14:0] dinit_address;
+    reg dinit_load;
+    reg [15:0] dinit_in;
+    wire [15:0] dm_in;
+    wire [14:0] dm_address;
+    wire dm_load;
+    reg dm_sel;
+    
 	wire [15:0] outM;
     wire writeM;
     wire [14:0] addressM;
@@ -35,17 +72,53 @@ module core_tb();
     wire [15:0] dreg;
     
     wire [15:0] q;
+    
+    reg [15:0] im_d;
+    reg [14:0] im_address;
+    reg im_we;
 
     instruction_memory instruction_memory (
         .clk(clk),
         .read_address(pc),
-        .q(q),
-        .d(16'b0),
-        .write_address(15'b0),
-        .we(1'b0)
+        .q(instruction),
+        .d(im_d),
+        .write_address(im_address),
+        .we(im_we)
     );
 
+    /*
     cpu cpu(
+        .clk(clk),
+        .reset(reset),
+        .inM(inM),
+        .instruction(instruction),
+        .outM(dm_in),
+        .writeM(dm_load),
+        .addressM(dm_address),
+        .pc(pc),
+        .areg(areg),
+        .dreg(dreg)
+    );
+    
+    data_mux data_mux(
+        .clk(clk),
+        .sel(dm_sel),
+        
+        .a_in(dinit_in),
+        .a_address(dinit_address),
+        .a_we(dinit_load),
+        
+        .b_in(dm_in),
+        .b_address(dm_address),
+        .b_we(dm_load),
+        
+        .out(outM),
+        .write(writeM),
+        .address(addressM)
+    );
+ */
+ 
+ cpu cpu(
         .clk(clk),
         .reset(reset),
         .inM(inM),
@@ -56,14 +129,6 @@ module core_tb();
         .pc(pc),
         .areg(areg),
         .dreg(dreg)
-    );
-    
-    data_int(
-        .clk(clk),
-        .in(dinit_in),
-        .address(dinit_address),
-        .load(dinit_load),
-        .out(dinit_out)
     );
 
     memory data_memory (
@@ -85,19 +150,49 @@ module core_tb();
     
     
     
-    $monitor ("%d %b %b %d %b %d %d %d %d %d %d %d %b %t", 
-                inM, instruction, reset, outM, writeM, addressM, pc, cpu.DRegister, dreg, areg, screen_address, screen_data, screen_we,  $time);
+    $monitor ("%d %b %b %b %b %d %d %d %d %d %d %d %b %t", 
+                inM, instruction, reset, outM, writeM, addressM, pc, cpu.DRegister,
+                dreg, areg, screen_address, screen_data, screen_we,  $time);
 
         clk = 1;
         reset = 0;
-  
-#1 reset = 1;  
-#1 reset = 0;
 
-//#2 
-//addressM = 15'b0;
-//writeM = 1'b1;
+// Initialize memory
+/*
+#2 
+dm_sel = 1;
+dinit_address = 15'd0;
+dinit_in = 15'd2;
+dinit_load = 1'b1;
 
+#2
+dm_sel = 0;
+dinit_load = 1'b0;
+*/
+
+// Load the program
+#2
+im_address = 15'd0;
+im_d = 16'b0000000000000010; // @2
+im_we = 1;
+#2
+im_address = 15'd1;
+im_d = 16'b1110110000010000; // D=A
+#2
+im_address = 15'd2;
+im_d = 16'b0000000000010111;
+#2
+im_address = 15'd3;
+im_d = 16'b1110001100000110;
+#2
+im_address = 15'd4;
+im_d = 16'b0000000000010000;
+
+#2
+im_we = 0;
+
+#2 reset = 1;  
+#2 reset = 0;
 
   
 	end
--- hack/testbench/core_wave.do
+++ hack/testbench/core_wave.do
@@ -4,19 +4,29 @@ add wave -noupdate /core_tb/clk
 add wave -noupdate /core_tb/reset
 add wave -noupdate /core_tb/inM
 add wave -noupdate /core_tb/instruction
-add wave -noupdate /core_tb/keyboard
-add wave -noupdate /core_tb/outM
+add wave -noupdate -radix binary /core_tb/outM
 add wave -noupdate /core_tb/writeM
 add wave -noupdate /core_tb/addressM
 add wave -noupdate -radix decimal /core_tb/pc
 add wave -noupdate /core_tb/screen_address
 add wave -noupdate /core_tb/screen_data
 add wave -noupdate /core_tb/screen_we
-add wave -noupdate /core_tb/areg
-add wave -noupdate /core_tb/dreg
-add wave -noupdate /core_tb/q
+add wave -noupdate -radix decimal /core_tb/areg
+add wave -noupdate -radix decimal /core_tb/dreg
+add wave -noupdate -divider ALU
+add wave -noupdate /core_tb/cpu/alu_inst/x
+add wave -noupdate /core_tb/cpu/alu_inst/y
+add wave -noupdate /core_tb/cpu/alu_inst/zx
+add wave -noupdate /core_tb/cpu/alu_inst/nx
+add wave -noupdate /core_tb/cpu/alu_inst/zy
+add wave -noupdate /core_tb/cpu/alu_inst/ny
+add wave -noupdate /core_tb/cpu/alu_inst/f
+add wave -noupdate /core_tb/cpu/alu_inst/no
+add wave -noupdate /core_tb/cpu/alu_inst/out
+add wave -noupdate /core_tb/cpu/alu_inst/zr
+add wave -noupdate /core_tb/cpu/alu_inst/ng
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {170 ns} 0}
+WaveRestoreCursors {{Cursor 2} {200 ns} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 208
 configure wave -valuecolwidth 100
@@ -32,4 +42,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {0 ns} {170 ns}
+WaveRestoreZoom {40 ns} {360 ns}
